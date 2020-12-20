5579148a35bb61adb6b6151419c10e4e3a4208c8
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-12-20 14:21:57 +0100

    Init Commit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
-
+from googleapiclient.discovery import build
 import os
-SECRET_KEY = os.getenv("KEY")
+print(os.getenv("KEY"))
+def main():
+    SECRET_KEY = 'AIzaSyAvYTXG3zm_YOLfI1h7Xsuy2LctFVqEB-Q'
+    service=build('youtube','v3',developerKey=SECRET_KEY)
+    request=service.channels().list(part='statistics' ,forUsername='schafer5')
+    response=request.execute()
+    return response
+
+a=main()
+
+print(a)
 
