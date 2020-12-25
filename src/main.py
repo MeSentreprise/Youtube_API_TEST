2631ff65ca4366ebe7beea5bcbfa9dbb2b827ed3
Sm-25 <63506657+MeSentreprise@users.noreply.github.com>
2020-12-25 22:37:09 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from googleapiclient.discovery import build
-import os
-print(os.getenv("KEY"))
+
 def main():
-    SECRET_KEY = 'AIzaSyAvYTXG3zm_YOLfI1h7Xsuy2LctFVqEB-Q'
+    SECRET_KEY = ''
     service=build('youtube','v3',developerKey=SECRET_KEY)
     request=service.channels().list(part='statistics' ,forUsername='schafer5')
     response=request.execute()
