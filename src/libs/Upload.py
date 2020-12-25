438bbc843b7d0f8b43b8b382150c21c0c4ee129c
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-12-25 22:30:20 +0100

    Finished

---
+++ src/libs/Upload.py
@@ -0,0 +1,32 @@
+import speech_recognition as micro
+import datetime
+from Google import Create_Service
+from googleapiclient.http import *
+from tkinter import *
+from tkinter import filedialog 
+
+def Upload_Video(Title,Description,selfDeclaredMadeForKids):
+    SCOPES=["https://www.googleapis.com/auth/youtube.upload"]
+    API_NAME = 'youtube'
+    API_VERSION = 'v3'
+    youtube=Create_Service("client_secret.json",API_NAME,API_VERSION,SCOPES)
+    video_properties={
+        'snippet':{
+            'title':Title,
+            'descripition':Description
+        },
+        'status':{
+            'privacyStatus': 'unlisted',
+            'selfDeclaredMadeForKids':selfDeclaredMadeForKids
+        }
+    }
+    filename = filedialog.askopenfilename(title="Select a Video File")
+    mediafile=MediaFileUpload(filename)
+    response_upload = youtube.videos().insert(
+        part='snippet,status',
+        body=video_properties,
+        media_body=mediafile
+    ).execute()
+
+
+
