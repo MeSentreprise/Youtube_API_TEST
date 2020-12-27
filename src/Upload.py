0de936e6f72e8936076ad44ab513b775461e8d86
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-12-27 14:17:56 +0100

    Voice Interaction Added

--- src/Upload.py
+++ src/Upload.py
@@ -4,30 +4,32 @@ from libs.Google import Create_Service
 from googleapiclient.http import *
 from tkinter import *
 from tkinter import filedialog 
-#Alos this file is here just to simplify my code.
+#Also this file is here just to simplify my code.
 
-def Upload_Video(Title,Description,selfDeclaredMadeForKids):
-    SCOPES=["https://www.googleapis.com/auth/youtube.upload"]
-    API_NAME = 'youtube'
-    API_VERSION = 'v3'
-    youtube=Create_Service("secret_file.json",API_NAME,API_VERSION,SCOPES)
-    video_properties={
-        'snippet':{
-            'title':Title,
-            'descripition':Description
+def Upload_Video(Title,Description,selfDeclaredMadeForKids,filename):
+    try:
+        SCOPES=["https://www.googleapis.com/auth/youtube.upload"]
+        API_NAME = 'youtube'
+        API_VERSION = 'v3'
+        youtube=Create_Service("secret_file.json",API_NAME,API_VERSION,SCOPES)
+        video_properties={
+            'snippet':{
+                'title':Title,
+                'descripition':Description
         },
-        'status':{
-            'privacyStatus': 'unlisted',
-            'selfDeclaredMadeForKids':selfDeclaredMadeForKids
+            'status':{
+                'privacyStatus': 'unlisted',
+                'selfDeclaredMadeForKids':selfDeclaredMadeForKids
+            }
         }
-    }
-    filename = filedialog.askopenfilename(title="Select a Video File")
-    mediafile=MediaFileUpload(filename)
-    response_upload = youtube.videos().insert(
-        part='snippet,status',
-        body=video_properties,
-        media_body=mediafile
-    ).execute()
+        mediafile=MediaFileUpload(filename)
+        response_upload = youtube.videos().insert(
+            part='snippet,status',
+            body=video_properties,
+            media_body=mediafile
+        ).execute()
+    except:
+        print("hahaha")
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,75 @@
 import Upload
+import pyttsx3
+import datetime
+engine = pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+import speech_recognition as sr
 if __name__ == "__main__":
-    Upload.Upload_Video('Test','Test',True)
+	the_hour=int(datetime.datetime.now().hour)
+	if the_hour>=0 and the_hour<12:
+		engine.say("Bonjour")					    
+		engine.runAndWait()
+	if the_hour>=12 and the_hour<18:
+		engine.say("Bonne après-midi")					    
+		engine.runAndWait()
+	if the_hour>=18 and the_hour<0:
+		engine.say("Bonsoir")					    
+		engine.runAndWait()
+	r = sr.Recognizer()
+	while(True):
+		with sr.Microphone() as source:
+			try:
+				audio = r.listen(source)
+				data = r.recognize_google(audio)
+				print(data)
+				if data!=0:
+					if "upload" in data:
+						phrases=["Le titre s'il vous plaît","La description s'il vous plaît","Est-ce-que le contenu est-il désigné aux enfants?"]
+						responses=[]
+						for a in phrases:
+							if a!="Le titre s'il vous plaît" or a!="La description s'il vous plaît":
+							    engine.say(a)
+							    engine.runAndWait()
+							    audio = r.listen(source)
+							    answer = r.recognize_google(audio)
+							    responses.append(answer)
+							    print(answer)
+							    if answer=="Yes":
+								    answer=True
+								    responses.append(answer)
+							    if answer=="No":
+								    answer=False
+								    responses.append(answer)
+							else:
+							    engine.say(a)
+							    engine.runAndWait()
+							    audio = r.listen(source)
+							    answer = r.recognize_google(audio)
+							    responses.append(answer)
+						title=responses[0]
+						description=responses[1]
+						Kids=responses[2]
+						print(responses)
+						Upload.Upload_Video(title,description,Kids)
+					elif "thanks" in data:
+						engine.say("Je vous en prie !")
+						engine.runAndWait()
+					elif "hi" in data or "hello" in data:
+						if "hi" in data:
+							engine.say("Salut")
+							engine.runAndWait()
+						if "hello" in data:
+								if the_hour>=0 and the_hour<12:
+									engine.say("Bonjour")					    
+									engine.runAndWait()
+								if the_hour>=12 and the_hour<18:
+									engine.say("Bonne après-midi")					    
+									engine.runAndWait()
+								if the_hour>=18 and the_hour<0:
+									engine.say("Bonsoir")					    
+									engine.runAndWait()
+			except:
+				print("Retry")
+
 
